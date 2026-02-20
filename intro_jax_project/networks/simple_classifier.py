c60b3cee12133b40cf0f7fb1f9825590bf005e6a
msaif00 <marisaif3@gmail.com>
2026-02-20 11:22:35 -0500

    addition of simple classifier

--- intro_jax_project/networks/simple_classifier.py
+++ intro_jax_project/networks/simple_classifier.py
@@ -12,8 +12,215 @@ sns.set()
 import jax
 import jax.numpy as jnp
 print("Using jax", jax.__version__)
+from flax import linen as nn
+import optax
+import torch.utils.data as data
 ## Progress bar
 from tqdm.auto import tqdm
 
-a = jnp.arange(10)
-print(a.device)
+
+class SimpleClassifier(nn.Module):
+    num_hidden: int
+    num_outputs:int
+
+    # def setup(self):
+    #     self.linear1 = nn.Dense(features=self.num_hidden)
+    #     self.linear2 = nn.Dense(features=self.num_outputs)
+    #with Flax can use nn.compact to create the network in a more compact way
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(features=self.num_hidden)(x)
+        x = nn.tanh(x)
+        x = nn.Dense(features=self.num_outputs)(x)
+        return x
+
+class XORDataset(data.Dataset):
+
+    def __init__(self, size, seed, std=0.1):
+        """
+        Inputs:
+            size - Number of data points we want to generate
+            seed - The seed to use to create the PRNG state with which we want to generate the data points
+            std - Standard deviation of the noise (see generate_continuous_xor function)
+        """
+        super().__init__()
+        self.size = size
+        self.np_rng = np.random.RandomState(seed=seed)
+        self.std = std
+        self.generate_continuous_xor()
+
+    def generate_continuous_xor(self):
+        # Each data point in the XOR dataset has two variables, x and y, that can be either 0 or 1
+        # The label is their XOR combination, i.e. 1 if only x or only y is 1 while the other is 0.
+        # If x=y, the label is 0.
+        data = self.np_rng.randint(low=0, high=2, size=(self.size, 2)).astype(np.float32)
+        label = (data.sum(axis=1) == 1).astype(np.int32)
+        # To make it slightly more challenging, we add a bit of gaussian noise to the data points.
+        data += self.np_rng.normal(loc=0.0, scale=self.std, size=data.shape)
+
+        self.data = data
+        self.label = label
+
+    def __len__(self):
+        # Number of data point we have. Alternatively self.data.shape[0], or self.label.shape[0]
+        return self.size
+
+    def __getitem__(self, idx):
+        # Return the idx-th data point of the dataset
+        # If we have multiple things to return (data point and label), we can return them as tuple
+        data_point = self.data[idx]
+        data_label = self.label[idx]
+        return data_point, data_label
+
+
+model = SimpleClassifier(num_hidden=8, num_outputs=1)
+print(model)
+
+rng = jax.random.PRNGKey(42)
+rng, inp_rng, init_rng = jax.random.split(rng, 3)
+inp = jax.random.normal(inp_rng, (8, 2))  # Batch size 8, input size 2
+# Initialize the model
+params = model.init(init_rng, inp)
+print(params)
+
+dataset = XORDataset(size=200, seed=42)
+print("Size of dataset:", len(dataset))
+print("Data point 0:", dataset[0])
+
+
+def visualize_samples(data, label):
+    data_0 = data[label == 0]
+    data_1 = data[label == 1]
+
+    plt.figure(figsize=(4,4))
+    plt.scatter(data_0[:,0], data_0[:,1], edgecolor="#333", label="Class 0")
+    plt.scatter(data_1[:,0], data_1[:,1], edgecolor="#333", label="Class 1")
+    plt.title("Dataset samples")
+    plt.ylabel(r"$x_2$")
+    plt.xlabel(r"$x_1$")
+    plt.legend()
+
+visualize_samples(dataset.data, dataset.label)
+plt.show()
+
+# This collate function is taken from the JAX tutorial with PyTorch Data Loading
+# https://jax.readthedocs.io/en/latest/notebooks/Neural_Network_and_Data_Loading.html
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple,list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+data_loader = data.DataLoader(dataset, batch_size=8, shuffle=True, collate_fn=numpy_collate)
+
+# next(iter(...)) catches the first batch of the data loader
+# If shuffle is True, this will return a different batch every time we run this cell
+# For iterating over the whole dataset, we can simple use "for batch in data_loader: ..."
+data_inputs, data_labels = next(iter(data_loader))
+
+# The shape of the outputs are [batch_size, d_1,...,d_N] where d_1,...,d_N are the
+# dimensions of the data point returned from the dataset class
+print("Data inputs", data_inputs.shape, "\n", data_inputs)
+print("Data labels", data_labels.shape, "\n", data_labels)
+
+## Optimizer
+optimizer = optax.sgd(learning_rate=0.1)
+
+from flax.training import train_state
+model_state = train_state.TrainState.create(apply_fn =model.apply,
+                                            params = params,
+                                            tx = optimizer)
+
+def calculate_loss_acc(state, params, batch):
+    data_input, labels = batch
+    # Obtain the logits and predictions of the model for the input data
+    logits = state.apply_fn(params, data_input).squeeze(axis=-1)
+    pred_labels = (logits > 0).astype(jnp.float32)
+    # Calculate the loss and accuracy
+    loss = optax.sigmoid_binary_cross_entropy(logits, labels).mean()
+    acc = (pred_labels == labels).mean()
+    return loss, acc
+
+batch = next(iter(data_loader))
+calculate_loss_acc(model_state, model_state.params, batch)
+
+@jax.jit  # Jit the function for efficiency
+def train_step(state, batch):
+    # Gradient function
+    grad_fn = jax.value_and_grad(calculate_loss_acc,  # Function to calculate the loss
+                                 argnums=1,  # Parameters are second argument of the function
+                                 has_aux=True  # Function has additional outputs, here accuracy
+                                )
+    # Determine gradients for current model, parameters and batch
+    (loss, acc), grads = grad_fn(state, state.params, batch)
+    # Perform parameter update with gradients and optimizer
+    state = state.apply_gradients(grads=grads)
+    # Return state and any other value we might want
+    return state, loss, acc
+
+@jax.jit  # Jit the function for efficiency
+def eval_step(state, batch):
+    # Determine the accuracy
+    _, acc = calculate_loss_acc(state, state.params, batch)
+    return acc
+
+train_dataset = XORDataset(size=2500, seed=42)
+train_data_loader = data.DataLoader(train_dataset, batch_size=128, shuffle=True, collate_fn=numpy_collate
+                                    )
+def train_model(state, data_loader, num_epochs=100):
+    # Training loop
+    for epoch in tqdm(range(num_epochs)):
+        for batch in data_loader:
+            state, loss, acc = train_step(state, batch)
+            # We could use the loss and accuracy for logging here, e.g. in TensorBoard
+            # For simplicity, we skip this part here
+    return state
+
+trained_model_state = train_model(model_state, train_data_loader, num_epochs=100)
+
+from flax.training import checkpoints
+
+checkpoints.save_checkpoint(ckpt_dir='/Users/marisaif/Documents/intro_jax/intro_jax_project/networks/my_checkpoints/',  # Folder to save checkpoint in
+                            target=trained_model_state,  # What to save. To only save parameters, use model_state.params
+                            step=100,  # Training step or other metric to save best model on
+                            prefix='my_model',  # Checkpoint file name prefix
+                            overwrite=True   # Overwrite existing checkpoint files
+                           )
+
+loaded_model_state = checkpoints.restore_checkpoint(
+                                             ckpt_dir='/Users/marisaif/Documents/intro_jax/intro_jax_project/networks/my_checkpoints/',   # Folder with the checkpoints
+                                             target=model_state,   # (optional) matching object to rebuild state in
+                                             prefix='my_model'  # Checkpoint file name prefix
+                                            )
+
+
+test_dataset = XORDataset(size=500, seed=123)
+# drop_last -> Don't drop the last batch although it is smaller than 128
+test_data_loader = data.DataLoader(test_dataset,
+                                   batch_size=128,
+                                   shuffle=False,
+                                   drop_last=False,
+                                   collate_fn=numpy_collate)
+
+def eval_model(state, data_loader):
+    all_accs, batch_sizes = [], []
+    for batch in data_loader:
+        batch_acc = eval_step(state, batch)
+        all_accs.append(batch_acc)
+        batch_sizes.append(batch[0].shape[0])
+    # Weighted average since some batches might be smaller
+    acc = sum([a*b for a,b in zip(all_accs, batch_sizes)]) / sum(batch_sizes)
+    print(f"Accuracy of the model: {100.0*acc:4.2f}%")
+
+eval_model(trained_model_state, test_data_loader)
+
+
+
+
+
+
+
+
